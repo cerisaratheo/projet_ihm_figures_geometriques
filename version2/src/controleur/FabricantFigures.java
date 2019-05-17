6851bc2ede9ea3143523a661999d124d6c5394f2
theo cerisara <cerisaratheo@gmail.com>
2019-05-17 15:06:44 +0200

    ajout FabricantFigures.java

---
+++ version2/src/controleur/FabricantFigures.java
@@ -0,0 +1,38 @@
+package controleur;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class FabricantFigures implements MouseListener {
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
