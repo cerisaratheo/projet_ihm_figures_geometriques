979302e967a042364f8bec633397f45e75b5f9a9
skrlucs <sakerlucas@gmail.com>
2019-05-24 10:32:46 +0200

    maj

--- version2/src/modele/FigureColoree.java
+++ version2/src/modele/FigureColoree.java
@@ -25,9 +25,6 @@ public abstract class FigureColoree {
 		
 	public void affiche(Graphics g) {
 		g.setColor(this.couleur);
-//		if (this.selected = true) {
-//			System.out.println("Les figures devraient s'afficher !");
-//		}
 	}
 	
 	public void selectionne() {
