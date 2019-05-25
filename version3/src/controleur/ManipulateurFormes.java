486706102271a1f9974d244587ad5009efd3ec61
theo cerisara <cerisaratheo@gmail.com>
2019-05-25 12:03:10 +0200

    maj

--- version3/src/controleur/ManipulateurFormes.java
+++ version3/src/controleur/ManipulateurFormes.java
@@ -1,5 +1,72 @@
 package controleur;
 
-public class ManipulateurFormes {
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
+import modele.DessinModele;
+import modele.FigureColoree;
+
+public class ManipulateurFormes implements MouseListener, MouseMotionListener{
+
+	private int last_x, last_y, indice, nbf, sel;
+	private boolean trans;
+	
+	public ManipulateurFormes(DessinModele dm) {
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public int nbFigures() {
+		
+	}
+	
+	public FigureColoree figureSelection() {
+		
+	}
+	
+	public void selectionProchaineFigure() {
+		
+	}
 }
