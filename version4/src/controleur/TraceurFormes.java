5dda2e66f742ef221df88f85e26ccbc93648d036
theo cerisara <cerisaratheo@gmail.com>
2019-05-30 20:58:20 +0200

    maj

---
+++ version4/src/controleur/TraceurFormes.java
@@ -0,0 +1,75 @@
+package controleur;
+
+import java.awt.event.*;
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class TraceurFormes extends JPanel implements MouseListener, MouseMotionListener{
+
+	private int last_x, last_y;
+	private Color couleurTrait = Color.black;
+	private Graphics g;
+	private ArrayList<Trait> tableauTraits = new ArrayList<Trait>();
+
+	public TraceurFormes(Graphics p_g) {
+		g=p_g;
+	}
+
+	public void setColor(Color c) {
+		couleurTrait=c;
+	}
+
+	public ArrayList<Trait> getTableauTraits() {
+		return tableauTraits;
+	}
+	
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		if (SwingUtilities.isLeftMouseButton(arg0)) {
+			g.setColor(couleurTrait);
+			g.drawLine(last_x, last_y, arg0.getX(), arg0.getY());
+			Trait t = new Trait(last_x, arg0.getX(),last_y, arg0.getY(), couleurTrait);
+			tableauTraits.add(t);
+			last_x = arg0.getX();
+			last_y = arg0.getY();
+		}
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		last_x = arg0.getX();
+		last_y = arg0.getY();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ version4/src/controleur/Trait.java
@@ -0,0 +1,59 @@
+package controleur;
+
+import java.awt.*;
+
+public class Trait {
+
+	private int debx, deby, finx, finy;
+	private Color couleur;
+	
+	public Trait(int dx,int fx,int dy,int fy,Color c) {
+		debx = dx;
+		deby = dy;
+		finx = fx;
+		finy = fy;
+		couleur = c;
+	}
+	
+	public int getDebx() {
+		return debx;
+	}
+	
+	public void setDebx(int debx) {
+		this.debx = debx;
+	}
+	
+	public int getDeby() {
+		return deby;
+	}
+	
+	public void setDeby(int deby) {
+		this.deby = deby;
+	}
+	
+	public int getFinx() {
+		return finx;
+	}
+	
+	public void setFinx(int finx) {
+		this.finx = finx;
+	}
+	
+	public int getFiny() {
+		return finy;
+	}
+	
+	public void setFiny(int finy) {
+		this.finy = finy;
+	}
+	
+	public Color getCouleur() {
+		return couleur;
+	}
+	
+	public void setCouleur(Color couleur) {
+		this.couleur = couleur;
+	}
+	
+	
+}
